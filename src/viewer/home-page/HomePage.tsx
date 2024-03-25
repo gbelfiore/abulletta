fa1cb4c0beea4fd5b88df6da5a4fa7645827d128
Giovanni Belfiore <g.belfiore@doveconviene.com>
2024-03-25 22:50:24 +0100

    wip

--- src/viewer/home-page/HomePage.tsx
+++ src/viewer/home-page/HomePage.tsx
@@ -1,4 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+
 const HomePage = () => {
+	useEffect(() => {
+		fetch("https://www.lottomatica.it/scommesse/sport/calcio/italia/serie-a/1-1577-93")
+			.then(function (response) {
+				switch (response.status) {
+					// status "OK"
+					case 200:
+						return response.text();
+					// status "Not Found"
+					case 404:
+						throw response;
+				}
+			})
+			.then(function (template) {
+				console.log(template);
+			})
+			.catch(function (response) {
+				// "Not Found"
+				console.log(response.statusText);
+			});
+	}, []);
 	return <div>ciaooo xxx</div>;
 };
 
