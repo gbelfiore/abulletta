0fda6785b2d5f2b706eef6e1bfa89ec53aa26afc
Giovanni Belfiore <g.belfiore@doveconviene.com>
2024-03-25 22:24:08 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    sperimentale: {
+    experimental: {
         serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium']
     }
 };
